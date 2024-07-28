4ddbd77837ec0538b4690b4a2af4a0162f4c8f8a
rsonaniya <rajatsonaniya28@gmail.com>
2024-07-28 08:37:58 +0530

    created get users api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -80,3 +80,44 @@ export const signOut = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getusers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = req.query.startIndex * 1 || 0;
+    const limit = req.query.limit * 1 || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .skip(startIndex)
+      .limit(limit)
+      .sort({ createdAt: sortDirection });
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({
+      totalUsers,
+      lastMonthUsers,
+      users: usersWithoutPassword,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
