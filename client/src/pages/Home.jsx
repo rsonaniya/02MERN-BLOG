31b513e8f113dc6cb988dce41122575b6f671f1f
rsonaniya <rajatsonaniya28@gmail.com>
2024-07-30 22:18:07 +0530

    complete homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,70 @@
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getchPosts = async () => {
+      try {
+        const res = await fetch("/api/post/getposts");
+        const data = await res.json();
+
+        if (res.ok) {
+          setPosts(data.posts);
+        } else {
+          console.log(data.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getchPosts();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          A space where thoughts, ideas, and stories come alive. Here,
+          you&apos;ll find a diverse range of topics that inspire, inform, and
+          entertain. Whether you&apos;re looking for insightful articles,
+          personal reflections, or simply a dose of daily inspiration,
+          you&apos;re in the right place. Dive in, explore, and join the
+          conversation!
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          See All Posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard post={post} key={post._id} />
+              ))}
+            </div>
+            <Link
+              to="/search"
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View All Posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
