0d5bdd5c1937af3f7c7a147975cdd7cb16006a1a
rsonaniya <rajatsonaniya28@gmail.com>
2024-08-03 12:57:03 +0530

    fixed sort issue n search api

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -30,7 +30,7 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = req.query.startIndex * 1 || 0;
     const limit = req.query.limit * 1 || 9;
-    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
