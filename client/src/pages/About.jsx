5354dd64ffe448db36cb1ec0ea24e25e7ca36f35
rsonaniya <rajatsonaniya28@gmail.com>
2024-08-03 12:01:57 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,39 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Rajat's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Welcome to our corner of the internet, where we share stories,
+              ideas, and inspiration. Our blog is a labor of love, created to
+              bring together a community of curious minds and passionate
+              individuals. Whether you're here to learn something new, find a
+              fresh perspective, or simply enjoy a good read, we hope you find
+              something that resonates with you.
+            </p>
+            <p className="">
+              At the heart of our blog is a commitment to quality content and
+              authentic voices. We believe that everyone has a story to tell and
+              that sharing these stories can bring people closer together. Our
+              diverse team of writers brings a wide range of experiences and
+              expertise, ensuring that there's always something interesting and
+              meaningful to explore.
+            </p>
+            <p className="">
+              Thank you for being a part of our journey. We're excited to have
+              you here and can't wait to share more with you. Stay tuned for
+              regular updates, engaging discussions, and plenty of inspiration.
+              Let's explore the world of ideas together!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,20 @@
+import CallToAction from "../components/CallToAction";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Welcome to our Projects page, where we proudly showcase our web
+        development work using the MERN stack. Here, you'll find a collection of
+        dynamic and responsive web applications that demonstrate our expertise
+        in MongoDB, Express.js, React, and Node.js. Each project highlights our
+        commitment to creating robust, scalable, and user-friendly solutions.
+        Dive in to explore how we leverage the power of MERN to bring innovative
+        ideas to life and drive digital transformation.
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
